0020142d7ec4d46dd48215ec2e0d612917ff7cdf
Nassim HAMRI <114926594+nassimhamri@users.noreply.github.com>
2025-08-05 14:58:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function safeQuerySelectorAll(selector) {
     return;
   }
 
-  fetch('/pages/nav.html')
+  fetch('pages/nav.html')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP ${response.status}`);
@@ -521,7 +521,7 @@ function initFooter() {
     return;
   }
 
-  fetch('/pages/footer.html')
+  fetch('pages/footer.html')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP ${response.status}`);
