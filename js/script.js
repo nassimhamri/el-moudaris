6570e3f2d0abdbea1eb4154ab57065e29fcebc94
Nassim HAMRI <114926594+nassimhamri@users.noreply.github.com>
2025-08-09 14:24:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function safeQuerySelectorAll(selector) {
     return;
   }
 
-  fetch('../pages/nav.html')
+  fetch('https://nassimhamri.github.io/el-moudaris/pages/nav.html')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP ${response.status}`);
@@ -521,7 +521,7 @@ function initFooter() {
     return;
   }
 
-  fetch('pages/footer.html')
+  fetch('https://nassimhamri.github.io/el-moudaris/pages/footer.html')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP ${response.status}`);
